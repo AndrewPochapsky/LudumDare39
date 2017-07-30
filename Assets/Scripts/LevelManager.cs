d5570dfd42cf04b1de902707fc5f1608cfd750b0
AndrewPochapsky <andrew.pochapsky@gmail.com>
2017-07-29 20:20:56 -0400

    MainMenu and loading screen

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,12 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelManager : MonoBehaviour {
+
+    public Transform panel;
+    public Slider slider;
+    public TextMeshProUGUI percent;
 
     public void LoadLevel(string level)
     {
         SceneManager.LoadScene(level);
     }
+
+    public void LoadLevelASync(string level)
+    {
+        StartCoroutine(LoadAsync(level));
+
+    }
+    
+    IEnumerator LoadAsync(string level)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(level);
+        panel.gameObject.SetActive(true);
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / 0.9f);
+            slider.value = progress;
+
+            percent.text = (progress * 100).ToString() + "%";
+
+            yield return null;
+        }
+    }
+
 }
